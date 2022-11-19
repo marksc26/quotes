6969d381b7a30a293c017f83a0219283eb15e33c
*Marcos <maromasi@hotmail.com>
2022-11-19 10:12:52 -0600

    3er commit

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -7,10 +7,11 @@ const QuoteBox = ({quote, getNewValues, backgroundObject, colorObject}) => {
         <p style={colorObject}>{quote.quote}</p>
         <h4 style={colorObject}>{quote.author}</h4>
         <section className="quoteBox-footer">
+          <div className="quoteBox-Button">
             <QuoteButton getNewValues={getNewValues} backgroundObject={backgroundObject}/>
-            <i style={colorObject} className='bx bxs-quote-alt-left'></i>
+          </div>
         </section>
-        
+        <i style={colorObject} className='bx bxs-quote-alt-left'></i>
 
     </article>
   )
--- src/components/QuoteButton.jsx
+++ src/components/QuoteButton.jsx
@@ -1,11 +1,11 @@
 
 const QuoteButton = ({getNewValues, backgroundObject}) => {
   return (
-    <div className="neumo">
+    
         <button onClick={getNewValues} style={backgroundObject}>
         <i  className='bx bx-right-arrow' ></i>
        </button>
-    </div>
+  
     
   )
 }
